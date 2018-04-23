03c065773942cf62fa927bbd6dd3bc5b6f1ee650
yaominguo <missgmy@yahoo.com>
2018-04-23 22:17:26 +0800

    header样式构建
    引入stylus，配置styles简写路径等

--- Travel-App/src/main.js
+++ Travel-App/src/main.js
@@ -4,8 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/styles/normalize.css'
-import './assets/styles/border.css'
+import 'styles/normalize.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
---
+++ Travel-App/src/pages/home/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      请输入搜索名称
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe62d;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl';
+  .header
+    height: 3rem
+    display: flex
+    align-items: center
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: 10%
+      float: left
+      text-align: center
+    .header-input
+      display: flex
+      padding: 0 1rem
+      flex: 1
+      background: #fff
+      border-radius: 3px
+      height: 80%
+      color: #ccc
+      align-items: center
+    .header-right
+      width: 20%
+      float: right
+      text-align: center
+</style>
