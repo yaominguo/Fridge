73204a44f3bb4c95524cdbec8ae691b17d080f2d
yaominguo <missgmy@yahoo.com>
2018-04-29 21:45:11 +0800

    Banner图片的渐隐渐现动画效果封装成公共组件

--- Travel-App/src/common/fade/Fade.vue
+++ Travel-App/src/common/fade/Fade.vue
@@ -6,13 +6,13 @@
 
 <script>
 export default {
-  name: 'Fade'
+  name: 'FadeAnimation'
 }
 </script>
 
 <style lang="stylus" scoped>
   .v-enter, .v-leave-to
     opacity: 0
-  .v-enter-active, v-leave-active
+  .v-enter-active, .v-leave-active
     transition: opacity 0.5s
 </style>
