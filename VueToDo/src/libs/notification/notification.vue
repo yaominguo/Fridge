73f815befb2b086f26918d2a27ef5bbc52329a15
yaominguo <missgmy@yahoo.com>
2018-05-08 20:57:50 +0800

    创建全局通知栏组件， 布局和样式

---
+++ VueToDo/src/libs/notification/notification.vue
@@ -0,0 +1,55 @@
+<template>
+  <transition name="fade">
+    <div class="container">
+      <span class="content">{{content}}</span>
+      <a class="btn" @click="handleClose">{{btn}}</a>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'Notification',
+  methods: {
+    handleClose (e) {
+      e.preventDefault()
+      this.$emit('close')
+    }
+  },
+  props: {
+    content: {
+      type: String,
+      required: true
+    },
+    btn: {
+      type: String,
+      default: '关闭'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .fade-enter-active, .fade-leave-active
+    transition opacity .5s
+  .fade-enter, .fade-leave-to
+    opacity 0
+  .container
+    display flex
+    background-color #000
+    color rgba(255,255,255,1)
+    align-items center
+    padding 20px
+    position fixed
+    min-width 280px
+    box-shadow 0 3px 5px -1px rgba(0,0,0,0.2)
+    flex-wrap wrap
+    transition all .3s
+    .content
+      padding 0
+    .btn
+      color #ff4081
+      padding-left 24px
+      margin-left auto
+      cursor pointer
+</style>
