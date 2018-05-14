22a00d0b9819449bb402019411a7b7b85c911e66
yaominguo <missgmy@yahoo.com>
2018-05-14 21:06:14 +0800

    加入vuex

--- VueToDo/src/main.js
+++ VueToDo/src/main.js
@@ -1,19 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
+import createStore from './store/store'
 import Notification from './libs/notification'
 import Tabs from './libs/tabs'
 import './assets/styles/global.styl'
 Vue.config.productionTip = false
 Vue.use(Notification)
 Vue.use(Tabs)
+Vue.use(Vuex)
+
+const store = createStore()
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  // el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  // components: { App },
+  // template: '<App/>'
+  render: (h) => h(App)
+}).$mount('#app')
