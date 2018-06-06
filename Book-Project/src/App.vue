aeae62d47c075d9e5d6d98dd180029038e8791c5
yaominguo <missgmy@yahoo.com>
2018-06-06 23:45:38 +0800

    发请求测试后台

--- Book-Project/src/App.vue
+++ Book-Project/src/App.vue
@@ -1,5 +1,20 @@
 <script>
+import config from './config'
 export default {
-  name: 'App'
+  name: 'App',
+  created () {
+    wx.request({
+      url: config.host + '/weapp/demo',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res)
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
+  }
 }
 </script>
---
+++ Book-Project/src/config.js
@@ -0,0 +1,6 @@
+// 配置项
+const host = 'http://localhost:5757'
+const config = {
+  host
+}
+export default config
