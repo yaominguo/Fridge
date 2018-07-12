f29a51940f0b0a3bb7b95db5212d585710333f78
yaominguo <missgmy@yahoo.com>
2018-07-12 23:46:00 +0800

    配置webpack使其支持jsx

--- react-cnode-teach/build/webpack.config.js
+++ react-cnode-teach/build/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
 
 module.exports = {
   // 入口文件
@@ -9,6 +10,25 @@ module.exports = {
   output: {
     filename: '[name].[hash].js', //这里的name是entry里的app
     path: path.join(__dirname, '../dist'),
-    publicPath: '/public', //帮助我们区分url是静态资源还是api请求
-  }
+    publicPath: '', //帮助我们区分url是静态资源还是api请求
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        exclude: [
+          path.join(__dirname, '../node_modules')
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // 生成html页面，同时将生成的js注入到html页面中
+    new HTMLPlugin()
+  ]
 }
