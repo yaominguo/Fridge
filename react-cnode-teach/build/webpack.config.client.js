79e9c5c88368c871a7243e05698757de0a078fdd
yaominguo <missgmy@yahoo.com>
2018-07-14 21:49:55 +0800

    热加载配置

--- react-cnode-teach/build/webpack.config.client.js
+++ react-cnode-teach/build/webpack.config.client.js
@@ -1,9 +1,11 @@
 const path = require('path')
+const webpack = require('webpack')
 const HTMLPlugin = require('html-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
 const config = {
+  mode: 'production',
   // 入口文件
   entry: {
     app: path.join(__dirname, '../client/app.js')
@@ -12,7 +14,7 @@ const config = {
   output: {
     filename: '[name].[hash].js', //这里的name是entry里的app
     path: path.join(__dirname, '../dist'),
-    publicPath: '/public', //帮助我们区分url是静态资源还是api请求
+    publicPath: '/public/', //帮助我们区分url是静态资源还是api请求
   },
   module: {
     rules: [
@@ -37,11 +39,18 @@ const config = {
   ]
 }
 if(isDev){
+  config.mode = 'development'
+  config.entry = {
+    app: [
+      'react-hot-loader/patch',
+      path.join(__dirname, '../client/app.js')
+    ]
+  }
   config.devServer = {
     host: '0.0.0.0', //这样可以支持localhost或ip地址的方式访问
     port: '8888',
     contentBase: path.join(__dirname, '../dist'),
-    // hot: true,
+    hot: true,
     overlay: { //报错弹窗更明显
       errors: true
     },
@@ -50,6 +59,7 @@ if(isDev){
       index: '/public/index.html'   //所有404的请求全部返回到这个html
     }
   }
+  config.plugins.push(new webpack.HotModuleReplacementPlugin())
 }
 
 module.exports = config
--- react-cnode-teach/client/app.js
+++ react-cnode-teach/client/app.js
@@ -1,5 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 import App from './App.jsx'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+const root = document.getElementById('root')
+
+ReactDOM.render(
+  <AppContainer>
+    <App/>
+  </AppContainer>, 
+  root
+)
+
+if(module.hot){
+  module.hot.accept('./App.jsx', () => {
+    const NextApp = require('./App.jsx').default
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp/>
+      </AppContainer>, 
+      root
+    )
+  })
+}
