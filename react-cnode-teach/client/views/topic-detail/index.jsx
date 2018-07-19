6983f7f6ddd30188a045a791649172dfe2cc176f
yaominguo <missgmy@yahoo.com>
2018-07-19 20:54:13 +0800

    目录结构搭建

---
+++ react-cnode-teach/client/views/topic-detail/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class TopicDetail extends React.Component {
+  componentDidMount() {
+    // todo
+  }
+
+  render() {
+    return (
+      <div>
+        This is topic detail
+      </div>
+    )
+  }
+}
---
+++ react-cnode-teach/client/views/topic-list/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class TopicList extends React.Component {
+  componentDidMount() {
+    // todo
+  }
+
+  render() {
+    return (
+      <div>
+        This is topic list
+      </div>
+    )
+  }
+}
