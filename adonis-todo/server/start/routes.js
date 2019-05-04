211ce3a2d3e8db5f33f6b02c62f85392b4ed0b5a
yaominguo <missgmy@yahoo.com>
2019-05-04 17:35:27 +0800

    task index & create

--- adonis-todo/server/start/routes.js
+++ adonis-todo/server/start/routes.js
@@ -19,8 +19,12 @@ const Route = use('Route')
 Route.group(() => {
   Route.post('auth/register', 'UserController.register')
   Route.post('auth/login', 'UserController.login')
+
   Route.get('projects', 'ProjectController.index').middleware('auth')
   Route.post('projects', 'ProjectController.create').middleware('auth')
   Route.delete('projects/:id', 'ProjectController.destroy').middleware('auth')
   Route.patch('projects/:id', 'ProjectController.update').middleware('auth')
+
+  Route.get('projects/:id/tasks', 'TaskController.index').middleware('auth')
+  Route.post('projects/:id/tasks', 'TaskController.create').middleware('auth')
 }).prefix('api')
