a6aff23a801d9f6762ba0fc14bec0b1c43c50719
yaominguo <missgmy@yahoo.com>
2019-05-16 22:08:26 +0800

    vue init

---
+++ adonis-todo/client/src/main.js
@@ -0,0 +1,18 @@
+import { sync } from 'vuex-router-sync';
+import Vuetify from 'vuetify';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+
+Vue.use(Vuetify);
+sync(store, router);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app');
