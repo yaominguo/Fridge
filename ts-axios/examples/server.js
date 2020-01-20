7d73b75e481582fee1baffd870eb60b2a4e3beb8
yaominguo <missgmy@yahoo.com>
2020-01-20 22:13:22 +0800

    尝试post请求带参数

--- ts-axios/examples/server.js
+++ ts-axios/examples/server.js
@@ -37,6 +37,18 @@ router.get('/base/get', (req, res) => {
 router.post('/base/post', (req, res) => {
   res.json(req.body)
 })
+router.post('/base/buffer', (req,res) => {
+  let msg = []
+  req.on('data', chunk => {
+    if (chunk) {
+      msg.push(chunk)
+    }
+  })
+  req.on('end', () => {
+    let buf = Buffer.concat(msg)
+    res.json(buf.toJSON())
+  })
+})
 
 app.use(router)
 
