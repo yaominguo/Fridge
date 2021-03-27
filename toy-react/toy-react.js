fa309b4099e6effb2fbc230d425b2299835cbca5
yaominguo <missgmy@yahoo.com>
2021-03-27 17:52:45 +0800

    引入vdom

--- toy-react/toy-react.js
+++ toy-react/toy-react.js
@@ -1,47 +1,6 @@
 const RENDER_TO_DOM = Symbol('renderToDOM')
 const RE_RENDER = Symbol('reRender')
 
-class ElementWrapper {
-  constructor(tag) {
-    this.root = document.createElement(tag)
-  }
-  setAttribute(name, value) {
-    if (name.match(/^on([\s\S]+)$/)) {
-      this.root.addEventListener(
-        RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()),
-        value
-      )
-      return
-    }
-    if (name === 'className') {
-      this.root.setAttribute('class', value)
-      return
-    }
-
-    this.root.setAttribute(name, value)
-  }
-  appendChild(component) {
-    const range = document.createRange()
-    range.setStart(this.root, this.root.childNodes.length)
-    range.setEnd(this.root, this.root.childNodes.length)
-    component[RENDER_TO_DOM](range)
-  }
-  [RENDER_TO_DOM](range) {
-    range.deleteContents()
-    range.insertNode(this.root)
-  }
-}
-
-class TextWrapper{
-  constructor(content) {
-    this.root = document.createTextNode(content)
-  }
-  [RENDER_TO_DOM](range) {
-    range.deleteContents()
-    range.insertNode(this.root)
-  }
-}
-
 export class Component {
   constructor() {
     this.props = Object.create(null)
@@ -53,6 +12,12 @@ export class Component {
   }
   appendChild(component) {
     this.children.push(component)
+  }
+  get vdom() {
+    return this.render().vdom
+  }
+  get vchildren() {
+    return this.children.map(child => child.vdom)
   }
   [RENDER_TO_DOM](range) {
     this._range = range
@@ -85,6 +50,88 @@ export class Component {
     }
     merge(this.state, newState)
     this[RE_RENDER]()
+  }
+}
+class ElementWrapper extends Component{
+  constructor(tag) {
+    super(tag)
+    this.tag = tag
+  }
+  get vdom() {
+    return this
+    // return {
+    //   tag: this.tag,
+    //   props: this.props,
+    //   children: this.children.map(child => child.vdom),
+    // }
+  }
+  // setAttribute(name, value) {
+  //   if (name.match(/^on([\s\S]+)$/)) {
+  //     this.root.addEventListener(
+  //       RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()),
+  //       value
+  //     )
+  //     return
+  //   }
+  //   if (name === 'className') {
+  //     this.root.setAttribute('class', value)
+  //     return
+  //   }
+
+  //   this.root.setAttribute(name, value)
+  // }
+  // appendChild(component) {
+  //   const range = document.createRange()
+  //   range.setStart(this.root, this.root.childNodes.length)
+  //   range.setEnd(this.root, this.root.childNodes.length)
+  //   component[RENDER_TO_DOM](range)
+  // }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents()
+    const root = document.createElement(this.tag)
+    for (const name in this.props) {
+      const value = this.props[name]
+      if (name.match(/^on([\s\S]+)$/)) {
+        root.addEventListener(
+          RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()),
+          value
+        )
+      } else {
+        if (name === 'className') {
+          root.setAttribute('class', value)
+        } else {
+          root.setAttribute(name, value)
+        }
+      }
+    }
+
+    for(const child of this.children) {
+      const childRange = document.createRange()
+      childRange.setStart(root, root.childNodes.length)
+      childRange.setEnd(root, root.childNodes.length)
+      child[RENDER_TO_DOM](childRange)
+    }
+    range.insertNode(root)
+  }
+}
+
+class TextWrapper extends Component{
+  constructor(content) {
+    super(content)
+    this.tag = '#text'
+    this.content = content
+    this.root = document.createTextNode(content)
+  }
+  get vdom() {
+    return this
+    // return {
+    //   tag: '#text',
+    //   content: this.content,
+    // }
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents()
+    range.insertNode(this.root)
   }
 }
 
